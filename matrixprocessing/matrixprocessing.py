a2b420896db6e64404a30213135090b547d6949a
ApanasiukFedor519st <f.m.apanasiuk@student.khai.edu>
2023-04-06 14:28:01 +0300

    matrixprocessing 1

---
+++ matrixprocessing/matrixprocessing.py
@@ -0,0 +1,28 @@
+def matrixenter():
+    print("Enter the size")
+    inp = input().split()
+    row = int(inp[0])
+    colums = int(inp[1])
+    listA = []
+
+    print("Enter the matrix")
+    while row > 0:
+        inp = input().split()[:colums]
+        listA.append(list(map(int,inp)))
+        row = row - 1
+    return listA
+
+def matrixprint(matrix):
+    for i in range(0,len(a)):
+        for k in range(0,len(a[0])):
+            print(matrix[i][k], end=" ")
+        print()
+a = matrixenter()
+b = matrixenter()
+if len(a)==len(b) and len(a[0]) == len(b[0]):
+    for i in range(0,len(a)):
+        for k in range(0,len(a[0])):
+            a[i][k]+=b[i][k]
+    matrixprint(a)
+else:
+    print("ERROR")
