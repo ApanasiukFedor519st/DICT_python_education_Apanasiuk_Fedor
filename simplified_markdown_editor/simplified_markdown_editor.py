d3a780b0fe1b934a578528902254d31bc0e04746
ApanasiukFedor519st <f.m.apanasiuk@student.khai.edu>
2023-04-07 16:37:54 +0300

    simplified_markdown_editor 2

--- simplified_markdown_editor/simplified_markdown_editor.py
+++ simplified_markdown_editor/simplified_markdown_editor.py
@@ -1,4 +1,6 @@
+text = ""
 while True:
+    line = ""
     print("Choose a formatter:")
     formatter = input()
     if formatter == "!help":
@@ -6,6 +8,37 @@ while True:
         continue
     elif formatter == "!done":
         break
+    elif formatter == "plain":
+        line = input("Text:")
+        text += f"{line} "
+    elif formatter == "bold":
+        line = input("Text:")
+        line = f"**{line}** "
+        text += line
+    elif formatter == "italic":
+        line = input("Text:")
+        line = f"*{line}* "
+        text += line
+    elif formatter == "header":
+        level = int(input('Level:>'))
+        while not 1 <= level <= 6:
+            level = int(input('The level should be within the range of 1 to 6.\nLevel:>'))
+        line = input("Text:")
+        line = f"{'#'*level} {line} "
+        text += line
+    elif formatter == "link":
+        label = input('Label:>')
+        url = input('URL:>')
+        line = f"[{label}]({url}) "
+        text += line
+    elif formatter == "inline-code":
+        line = input("Text:")
+        line = f"`{line}` "
+        text += line
+    elif formatter == "new-line":
+        text += "\n"
     else:
         print("Unknown formatting type or command")
         continue
+    print(text)
+
