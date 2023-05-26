c51fb6f0a6fbead8a29c7c17188ffcfc530386d4
ApanasiukFedor519st <f.m.apanasiuk@student.khai.edu>
2023-05-26 12:43:59 +0300

    currency_exchange 1
    currency_exchange 1
    
    currency_exchange 3
    
    currency_exchange 4

---
+++ currency_exchange/currency_exchange.py
@@ -0,0 +1,66 @@
+import requests
+import json
+
+
+class ValueConverter:
+    def __init__(self):
+        self.__cache: dict = {}
+
+    def algorithm(self):
+        while True:
+            cur1 = input('Setup the value,  please: ')
+
+            if cur1 == 'USD' or 'EUR':
+                break
+
+            if self.__get_curs(cur1, 'usd') is None:
+                print("Sorry, type the right value")
+            else:
+                self.__get_curs(cur1, 'usd')
+                self.__get_curs(cur1, 'eur')
+                break
+
+        while True:
+            cur2: str = input('Type the value to exchange: ').lower()
+
+            while True:
+                try:
+                    amount: float = float(input('Type the amount of money you have: '))
+                except ValueError:
+                    print("Please, type correct value with numbers")
+                else:
+                    break
+
+            print("Checking the cache...")
+            if cur2 not in self.__cache.keys():
+                print("Sorry, but it is not in cache!")
+                print(f"You received {self.__calculation(self.__get_curs(cur1, cur2), amount)} {cur2.upper()}")
+
+            else:
+                print("It is in cache!")
+                print(f"You received {self.__calculation(self.__cache[cur2], amount)} {cur2.upper()}")
+
+    def __get_curs(self, cur1: str, cur2: str) -> [int, None]:
+        try:
+            json_file = json.loads(requests.get(f"http://www.floatrates.com/daily/{cur1}.json").text)
+        except json.decoder.JSONDecodeError:
+            return None
+
+        try:
+            cur_val = json_file[cur2]["rate"]
+        except KeyError:
+            return None
+
+        self.__cache[cur2] = cur_val
+        return cur_val
+
+    @staticmethod
+    def __calculation(val_cur: [float, int], money: [float, int]) -> [float, int, str]:
+        if val_cur is None:
+            return "nothing, because this value is not exist or the same with first value -->"
+
+        return round(val_cur * money, 2)
+
+
+v = ValueConverter()
+v.algorithm()
